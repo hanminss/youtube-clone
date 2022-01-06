08df11a011e75a28c54174451b92161a9db71d79
HanMinSS <gksals6464@naver.com>
2022-01-06 13:49:27 +0900

    ✅  show video list with title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import VideoList from "./components/video_list/video_list";
+import { API_KEY } from "./constants/API_KEY";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [videos, setVideos] = useState([]);
+  const [name, setName] = useState("ellie");
+
+  // [] 이기 때문에 마운트 되었을때 한번만
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${API_KEY}`,
+      requestOptions
+    )
+      .then((response) => response.json()) // txt -> json 으로 바꿔주는 게 좋다.
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
